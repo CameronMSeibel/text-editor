d41fa6f8b45839bffaf87825ff3717cef3d7fd2a
cmseibel <cam.m.seib@gmail.com>
2022-03-25 10:00:02 -0700

    Write custom PWA install functionality.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,34 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let installPrompt;
+
+
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    installPrompt = event;
+    butInstall.style.visibility = "visible";
+
+    
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if(installPrompt !== null){
+        installPrompt.prompt();
+        const {outcome} = await installPrompt.userChoice;
+        console.log(outcome)
+        if(outcome === "accepted"){
+            butInstall.setAttribute("disabled", true);
+            butInstall.style.visibility = "hidden";
+        }
+    }
+});
+
+
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log("App installed.")
+});
